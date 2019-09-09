21d8ba6881ede13fd5ba2c8dccc7645b57a46941
sam <huapox@126.com>
2019-09-09 12:13:02 +0800

    alter k3s-prs's notes

--- t-k3s/build.sh
+++ t-k3s/build.sh
@@ -4,17 +4,25 @@ set +e
 
 dir=/go/src/github.com/rancher/k3s/
 mkdir -p $dir
-git clone https://github.com/huapox/fk-k3s-master $dir
+git clone https://github.com/huapox/fk-k3s-custom $dir
 cd $dir && ls -lh
+git branch -a
 
 
 
 #make
 cd scripts
-#bash ./release-arm  ##./ci
+#bash ./release-arm  #./ci  #./clean
 bash ./download
 bash ./build
-bash ./package-cli
+
+#./package
+##COPY --from=src /go/src/github.com/rancher/k3s/dist/artifacts/k3s (ff: att-k3s-prs)
+##ADD build/out/data.tar.gz (package/Dockerfile)
+bash ./package-cli  ##
+##dind: outer-multi docker-build (same: avoid edit package-image_no-push, package/Dockerfile)
+#bash ./package-image  
+#bash ./package-airgap ##no build here
 
 
 rm -f /build.sh
