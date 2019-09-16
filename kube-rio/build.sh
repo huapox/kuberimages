ff8abb1d140b1e9b6e436234d200e2dfd8604ac8
sam <huapox@126.com>
2019-09-16 23:54:06 +0800

    v040

--- kube-rio/build.sh
+++ kube-rio/build.sh
@@ -6,7 +6,7 @@ apk add bash xterm tmux tree wget curl sed sudo shadow |grep OK
 mkdir -p /down
 cd /down
 	##rio
-	ver=v0.5.0-alpha.1
+	ver=v0.4.0 #v0.5.0-alpha.1
 	src=https://github.com/rancher/rio/releases/download/$ver/rio-linux-amd64
 	dest=/down/rio
 	curl -L $src -o $dest; chmod +x $dest;
